1231d0ef4dd5a0ab0721e7c5e0736b944d8e7a23
Robert Massaioli <rmassaioli@atlassian.com>
2024-10-11 11:17:16 +1100

    Refactoring the repository to support multiple instances.

--- generate-all.bash
+++ generate-all.bash
@@ -1,10 +1,14 @@
+TEMPLATE_DIR="${PWD}/templates"
+
+bash factorio-script.bash ${i} > "${TEMPLATE_DIR}/1-server/update-factorio-servers.bash"
+
 for i in 5 10 15 20 40 60
 do
   echo "Generating $i..."
-  SERVER_DIR="${PWD}/templates/${i}-servers"
+  SERVER_DIR="${TEMPLATE_DIR}/${i}-servers"
   echo "Generating to ${SERVER_DIR}"
   mkdir -p "${SERVER_DIR}"
-  bash parameterised.bash ${i} > "${SERVER_DIR}/cf.yml"
-  bash factorio-script.bash ${i} > "${SERVER_DIR}/update-factorio-servers.bash"
+  bash src/parameterised.bash ${i} > "${SERVER_DIR}/cf.yml"
+  bash src/factorio-script.bash ${i} > "${SERVER_DIR}/update-factorio-servers.bash"
   echo "Generated $i!"
 done
